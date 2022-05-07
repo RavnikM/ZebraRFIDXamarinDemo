c04adf0255ff5cdd6cff89be067eaa37547b0a45
Marko Ravnik <markoravnik@gmail.com>
2022-05-07 23:08:54 +0200

    init test

--- ZebraRFIDXamarinDemo/ZebraRFIDXamarinDemo/Views/ApiTest.xaml.cs
+++ ZebraRFIDXamarinDemo/ZebraRFIDXamarinDemo/Views/ApiTest.xaml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace ZebraRFIDXamarinDemo.Views
 {
@@ -17,10 +19,21 @@ namespace ZebraRFIDXamarinDemo.Views
             InitializeComponent();
             
         }
-
-        public void on_get_clicked(object sender, EventArgs e)
+        
+        public async void on_get_clicked(object sender, EventArgs e)
         {
             System.Console.WriteLine("Klik na GET");
+
+            HttpClient _client;
+            _client = new HttpClient();
+            HttpResponseMessage response = await _client.GetAsync("http://api.open-notify.org/iss-now.json?callback=?");
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                System.Console.WriteLine(content);
+                
+                //RecivedDataEditorGET.Text = content;
+            }
         }
 
 
